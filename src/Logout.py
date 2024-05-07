ba76fb20f0bd03abc3d07e406a7823b54da45885
usernamedarren <19623168@std.stei.itb.ac.id>
2024-05-07 11:03:35 +0700

    feat: Logout function

---
+++ src/Logout.py
@@ -0,0 +1,6 @@
+def logout (loginValid):
+  if loginValid == False :
+    print('Logout GAGAL!')
+    print('Anda belum login, silakan login terlebih dahulu')
+
+# contoh penggunaan function : logout(False)
