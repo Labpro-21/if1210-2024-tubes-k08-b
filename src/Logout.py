7be4c014685042dd2a10434aa6a968de70e4ed87
usernamedarren <19623168@std.stei.itb.ac.id>
2024-05-14 00:54:53 +0700

    fix: Add command to logout if loginValid is True

--- src/Logout.py
+++ src/Logout.py
@@ -2,5 +2,8 @@ def logout (loginValid):
   if loginValid == False :
     print('Logout GAGAL!')
     print('Anda belum login, silakan login terlebih dahulu')
+  else :
+    loginValid = False
 
-# contoh penggunaan function : logout(False)
+# contoh penggunaan function : 
+logout(True)
