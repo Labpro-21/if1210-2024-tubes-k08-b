c521303e546cdb931a6fde2cf180de5936f2b114
Ghifaruru <16523058@std.stei.itb.ac.id>
2024-05-20 08:43:37 +0700

    fix : fix an error in exit

--- src/exit.py
+++ src/exit.py
@@ -1,10 +1,10 @@
 from src.load import save
 def exit(user,monsterinventory,itemshop,monster,monstershop,iteminventory):
-    confirmExit = input(("Apakah Anda mau melakukan penyimpanan file yang sudah diubah (y/n)"))
-    while confirmExit != "y" and confirmExit != "n":
-        confirmExit = input(print("Apakah Anda mau melakukan penyimpanan file yang sudah diubah (y/n)"))
-    if confirmExit == "y":
+    confirmExit = input(("Apakah Anda mau melakukan penyimpanan file yang sudah diubah (y/n) :"))
+    while confirmExit.lower() != "y" and confirmExit.lower() != "n":
+        confirmExit = input(("Masukkan y/n saja :"))
+    if confirmExit.lower() == "y":
         save(iteminventory,monster,user,monstershop,itemshop,monsterinventory)
         print("Program berhasil disave dan ditutup!")
-    else: # confirmExit =="n
+    elif confirmExit.lower() == "n" : 
         print("Program berhasil ditutup!")
